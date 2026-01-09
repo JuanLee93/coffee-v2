9dc9364210a844c4f73a7fc0e895bc736c9e7711
juan <ewndks@gmail.com>
2026-01-09 22:43:03 +0900

    2026.01.09 이주안 1. p6spy 설정수정

--- src/main/java/com/example/coffee_v2/config/P6SpyConfig.java
+++ src/main/java/com/example/coffee_v2/config/P6SpyConfig.java
@@ -8,9 +8,9 @@ import org.hibernate.engine.jdbc.internal.FormatStyle;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.ClassUtils;
 
+import java.util.Arrays;
 import java.util.Locale;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.Optional;
 
 @Configuration
 public class P6SpyConfig implements MessageFormattingStrategy {
@@ -21,34 +21,27 @@ public class P6SpyConfig implements MessageFormattingStrategy {
     }
 
     @Override
-    public String formatMessage(int connectionId, String now, long elapsed, String category,
-                                String prepared, String sql, String url) {
-        String formattedSql = formatSql(category, sql);
-        String stack = stackTrace();
-
-        // stackTrace가 있으면 한 줄 띄우고 SQL 출력
-        if (!stack.isEmpty()) {
-            return String.format("[%s] | %d ms |\n%s\n%s", category, elapsed, stack, formattedSql);
-        } else {
-            return String.format("[%s] | %d ms | %s", category, elapsed, formattedSql);
-        }
+    public String formatMessage(int connectionId, String now, long elapsed, String category, String prepared, String sql, String url) {
+        return String.format("[%s] | %d ms | %s", category, elapsed, formatSql(category, sql));
     }
 
-    private String stackTrace() {
-        return Stream.of(new Throwable().getStackTrace())
-                .filter(t -> t.toString().startsWith("com.example.coffee_v2") && !t.toString().contains(
-                        ClassUtils.getUserClass(this).getName()))
-                .map(StackTraceElement::toString)
-                .collect(Collectors.joining("\n"));
+    private static String stackTrace() {
+        StackTraceElement[] elements = new Throwable().getStackTrace();
+        String className = ClassUtils.getUserClass(P6SpyConfig.class).getName();
+        Optional<StackTraceElement> firstArray = Arrays.stream(elements)
+                .filter(t -> t.toString().startsWith("com.example.coffee_v2") && !t.toString().contains(className))
+                .findFirst();
+        return firstArray.map(StackTraceElement::toString).orElse("");
     }
 
     private String formatSql(String category, String sql) {
         if (sql != null && !sql.trim().isEmpty() && Category.STATEMENT.getName().equals(category)) {
-            String trimmedSql = sql.trim().toLowerCase(Locale.ROOT);
-            return stackTrace() + (trimmedSql.startsWith("create") || trimmedSql.startsWith("alter")
-                    || trimmedSql.startsWith("comment")
-                    ? FormatStyle.DDL.getFormatter().format(sql)
-                    : FormatStyle.BASIC.getFormatter().format(sql));
+            String trimmedSQL = sql.trim().toLowerCase(Locale.ROOT);
+            if (trimmedSQL.startsWith("create") || trimmedSQL.startsWith("alter") || trimmedSQL.startsWith("comment")) {
+                sql = stackTrace() + FormatStyle.DDL.getFormatter().format(sql);
+            } else {
+                sql = stackTrace() + FormatStyle.BASIC.getFormatter().format(sql);
+            }
         }
         return sql;
     }
